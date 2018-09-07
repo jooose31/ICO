19c0f86353232473a243a86ddcbf1061cca019a6
jose <josed@galileo.edu>
2018-09-07 13:33:20 -0600

    addActivity

--- app/src/main/java/com/example/jdavi/ico/addActivity.java
+++ app/src/main/java/com/example/jdavi/ico/addActivity.java
@@ -18,7 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widge
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
